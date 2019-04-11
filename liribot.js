3867e86f793e3f32579dc11e8e1a32fdcc9b8019
toshyamsundar <toshyamsundar@yahoo.com>
2019-04-11 01:08:43 -0400

    Added function to call spotify API

--- liribot.js
+++ liribot.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
-let keys = require("./keys");
-let fs = require("fs");
-let axios = require("axios");
-let moment = require("moment");
+const keys = require("./keys");
+const fs = require("fs");
+const axios = require("axios");
+const moment = require("moment");
+const Spotify = require("node-spotify-api");
 
 let inputArguments = process.argv.slice(2);
 let inputCommand = inputArguments[0];
 let inputSearchString = inputArguments.splice(1).join(" ");
 let fileLines = [];
+
+let spotify = new Spotify(keys.spotify);
 
 fs.readFile("./random.txt", "utf8", function(error, data) {
   if (error) {
@@ -24,12 +27,37 @@ fs.readFile("./random.txt", "utf8", function(error, data) {
 let getBandsInTown = inputSearchString => {
   let queryString = `https://rest.bandsintown.com/artists/${inputSearchString}/events?app_id=codingbootcamp`;
 
-  axios.get(queryString).then(function(response) {
-    response.data.forEach(event => {
-      console.log(`Name: ${event.venue.name}`);
-      console.log(`Location: ${event.venue.city}, ${event.venue.country}`);
-      let eventDate = moment(event.datetime).format("MM/DD/YYYY");
-      console.log(`Date: ${eventDate}`);
+  axios
+    .get(queryString)
+    .then(function(response) {
+      response.data.forEach(event => {
+        console.log(`Name: ${event.venue.name}`);
+        console.log(`Location: ${event.venue.city}, ${event.venue.country}`);
+        let eventDate = moment(event.datetime).format("MM/DD/YYYY");
+        console.log(`Date: ${eventDate}`);
+        console.log(`More Info: ${event.url}`);
+        console.log(`-------------------------------------------------------`);
+      });
+    })
+    .catch(function(err) {
+      console.log(`Error: ${err}`);
+    });
+};
+
+let getSpotifyDetails = inputSearchString => {
+  spotify.search({ type: "track", query: inputSearchString, limit: 5 }, function(err, response) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+    response.tracks.items.forEach(song => {
+      let artistNames = [];
+      console.log(`Song: ${song.name}`);
+      console.log(`Album: ${song.album.name}`);
+      song.artists.forEach(artist => {
+        artistNames.push(artist.name);
+      });
+      console.log(`Artists: ${artistNames}`);
+      console.log(`Preview: ${song.preview_url}`);
       console.log(`-------------------------------------------------------`);
     });
   });
@@ -40,7 +68,7 @@ switch (inputCommand) {
     getBandsInTown(inputSearchString);
     break;
   case "spotify-this-song":
-    console.log("Spotify");
+    getSpotifyDetails(inputSearchString);
     break;
   case "movie-this":
     console.log("OMDB API");
