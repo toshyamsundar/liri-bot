295e36310aed8b4af7ed20cf43bd3b0dfb65f307
toshyamsundar <toshyamsundar@yahoo.com>
2019-04-10 23:27:22 -0400

    Added function to make BandsInTown API call and get event details

--- liribot.js
+++ liribot.js
@@ -1,26 +1,43 @@
 require("dotenv").config();
 let keys = require("./keys");
 let fs = require("fs");
+let axios = require("axios");
+let moment = require("moment");
 
 let inputArguments = process.argv.slice(2);
 let inputCommand = inputArguments[0];
 let inputSearchString = inputArguments.splice(1).join(" ");
+let fileLines = [];
 
 fs.readFile("./random.txt", "utf8", function(error, data) {
   if (error) {
     console.log(error);
   } else {
-    var lineArr = data.split(",");
+    fileLines = data.split(",");
 
-    lineArr.forEach(element => {
-      console.log(element);
-    });
+    // fileLines.forEach(line => {
+    //   console.log(line);
+    // });
   }
 });
 
+let getBandsInTown = inputSearchString => {
+  let queryString = `https://rest.bandsintown.com/artists/${inputSearchString}/events?app_id=codingbootcamp`;
+
+  axios.get(queryString).then(function(response) {
+    response.data.forEach(event => {
+      console.log(`Name: ${event.venue.name}`);
+      console.log(`Location: ${event.venue.city}, ${event.venue.country}`);
+      let eventDate = moment(event.datetime).format("MM/DD/YYYY");
+      console.log(`Date: ${eventDate}`);
+      console.log(`-------------------------------------------------------`);
+    });
+  });
+};
+
 switch (inputCommand) {
   case "concert-this":
-    console.log("Bands In Town");
+    getBandsInTown(inputSearchString);
     break;
   case "spotify-this-song":
     console.log("Spotify");
